d1ec030e29338446490cdf555278599aa9cf5f7a
trevisharp <leonardotrevisansilio@gmail.com>
2025-07-18 13:45:41 -0300

    migrations applied

---
+++ Migrations/20250718164057_InitialCreate.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeleQuemLimpa.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Days",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Done = table.Column<bool>(type: "bit", nullable: false),
+                    DoneAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Days", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Divas",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Divas", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DayDiva",
+                columns: table => new
+                {
+                    DaysID = table.Column<int>(type: "int", nullable: false),
+                    DivasID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DayDiva", x => new { x.DaysID, x.DivasID });
+                    table.ForeignKey(
+                        name: "FK_DayDiva_Days_DaysID",
+                        column: x => x.DaysID,
+                        principalTable: "Days",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DayDiva_Divas_DivasID",
+                        column: x => x.DivasID,
+                        principalTable: "Divas",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DayDiva_DivasID",
+                table: "DayDiva",
+                column: "DivasID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DayDiva");
+
+            migrationBuilder.DropTable(
+                name: "Days");
+
+            migrationBuilder.DropTable(
+                name: "Divas");
+        }
+    }
+}
